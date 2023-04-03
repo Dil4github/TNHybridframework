be536b36956842526b0e7a22466f67798d47abd5
Dil4github <dlrisrpl@gmail.com>
2023-04-03 12:03:32 -0400

    first commit

---
+++ TNHybridframework_Panda/src/main/java/com/JavaAutomationCode/TNHybridframework_Panda/App.java
@@ -0,0 +1,13 @@
+package com.JavaAutomationCode.TNHybridframework_Panda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
